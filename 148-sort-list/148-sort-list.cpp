9a617dda91d5cd9c7d8a6443e556fe049df840f1
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-01-05 19:36:48 +0530

    Time: 159 ms (16.56%), Space: 30.2 MB (65.06%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -10,22 +10,35 @@
  */
 class Solution {
 public:
+    ListNode* merge(ListNode* a,ListNode* b)
+    {
+        if(!a)return b;
+        if(!b)return a;
+        
+        if(a->val <= b->val)
+        {
+            a->next=merge(a->next,b);
+            return a;
+        }
+        else
+        {
+            b->next=merge(a,b->next);
+            return b;
+        }
+    }
+    
     ListNode* sortList(ListNode* head) {
-        ListNode* temp=head;
-        vector<int> p;
-        while(temp)
+        if(!head || !head->next)return head;
+        ListNode *slow=head, *fast=head->next;
+        while(fast && fast->next)
         {
-            p.push_back(temp->val);
-            temp=temp->next;
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        temp=head;
-        sort(p.begin(),p.end());
-        int i=0;
-        while(i<p.size())
-        {
-            temp->val=p[i++];
-            temp=temp->next;
-        }
-        return head;
+        
+        fast=slow->next;
+        slow->next=NULL;
+        return merge(sortList(head),sortList(fast));
+        
     }
 };
