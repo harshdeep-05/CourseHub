93e48efbaf23f4e9a1fd158af7a75dc5f54d0c50
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-01-15 19:29:41 +0530

    Time: 189 ms (37.06%), Space: 11.2 MB (64.01%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -6,17 +6,19 @@ public:
         if(sum%2)return 0;
         sum/=2;
         
-        vector<bool> dp(sum+1,false);
-        dp[0]=true;
+        bool dp[n+1][sum+1];
+        for(int i=0;i<=sum;i++)dp[0][i]=0;
+        for(int i=0;i<=n;i++)dp[i][0]=1;
         
-        for(auto i:nums)
+        for(int i=1;i<=n;i++)
         {
-            for(int j=sum;j>=i;j--)
+            for(int j=0;j<=sum;j++)
             {
-                dp[j]=dp[j]||dp[j-i];
+                if(nums[i-1]<=j)dp[i][j]=dp[i-1][j-nums[i-1]]||dp[i-1][j];
+                else dp[i][j]=dp[i-1][j];
             }
         }
-        return dp[sum];
         
+        return dp[n][sum];
     }
 };
