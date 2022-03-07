c399f735d9c8aa94a0480c65ae29842959ab5d3f
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-03-07 13:23:36 +0530

    Time: 495 ms (81.54%), Space: 286.7 MB (63.23%) - LeetHub

--- 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.cpp
+++ 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.cpp
@@ -1,43 +1,26 @@
 class Solution {
 public:
     vector<int> maxScoreIndices(vector<int>& nums) {
-        unordered_map<int,int> m;
-        int n=nums.size(),mx=0;
-        /*
+        int right=0,n=nums.size(),mx=0,left=0;
+        for(auto i:nums)right+=i;
+        
+        vector<int> ans;
         for(int i=0;i<=n;i++)
         {
-            int l=i-1,r=i,left=0,right=0;
-            while(l>=0)left+=1-nums[l--];
-            while(r<n)right+=nums[r++];
-            m[i]=left+right;
-            mx=max(m[i],mx);
-        }
-        */
-        vector<int> left(n),right(n);
-        left[0]=1-nums[0];
-        for(int i=1;i<n;i++)
-        {
-            left[i]=1-nums[i]+left[i-1];
-        }
-        right[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--)
-        {
-            right[i]=nums[i]+right[i+1];
-        }
-        m[0]=right[0];
-        m[n]=left[n-1];
-        mx=max(m[0],m[n]);
-        for(int i=1;i<n;i++)
-        {
-            m[i]=left[i-1]+right[i];
-            mx=max(m[i],mx);
-        }
-        vector<int> ans;
-        for(auto i:m)
-        {
-            if(i.second==mx)ans.push_back(i.first);
+            if(left+right==mx)ans.push_back(i);
+            else if(left+right>mx)
+            {
+                mx=left+right;
+                ans.clear();
+                ans.push_back(i);
+            }
+            
+            if(i!=n)
+            {
+                if(nums[i]==1)right--;
+                else left++;
+            }
         }
         return ans;
-        
     }
 };
