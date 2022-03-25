cbe599004811f18771e9b59fffa81c35ee2cb52b
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-03-25 12:16:00 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (91.62%) - LeetHub

--- 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
-    vector<int> diffWaysToCompute(string s) {
+    vector<int> solve(string s,unordered_map<string,vector<int>> &m)
+    {
         vector<int> ans;
         int n=s.size();
         for(int i=0;i<n;i++)
         {
             if(s[i]=='+'|| s[i]=='-'|| s[i]=='*')
             {
-                vector<int> l=diffWaysToCompute(s.substr(0,i));
-                vector<int> r=diffWaysToCompute(s.substr(i+1));
+                vector<int> l,r;
+                string sub=s.substr(0,i);
+                if(m.find(sub)!=m.end())
+                {
+                    l=m[sub];
+                }
+                else l=solve(sub,m);
+                
+                string sub2=s.substr(i+1);
+                if(m.find(sub2)!=m.end())
+                {
+                    r=m[sub2];
+                }
+                else r=solve(sub2,m);
                 
                 for(auto x:l)
                 {
@@ -26,6 +39,11 @@ public:
             for(auto i:s)x=x*10+i-'0';
             ans.push_back(x);
         }
+        m[s]=ans;
         return ans;
+    }
+    vector<int> diffWaysToCompute(string s) {
+        unordered_map<string,vector<int>> m;
+        return solve(s,m);
     }
 };
