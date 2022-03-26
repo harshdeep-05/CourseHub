2d121fd14ab3ddd70c2b2fc4d25591f35f54cf25
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-03-26 18:39:21 +0530

    Time: 103 ms (8.33%), Space: 41.4 MB (80.22%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
@@ -11,43 +11,17 @@
  */
 class Solution {
 public:
+    int ans=0;
+    void dfs(TreeNode* child,TreeNode* par,TreeNode* gp)
+    {
+        if(!child)return;
+        if(gp && gp->val%2==0)ans+=child->val;
+        
+        dfs(child->left,child,par);
+        dfs(child->right,child,par);
+    }
     int sumEvenGrandparent(TreeNode* root) {
-        unordered_map<TreeNode*,TreeNode*> m;
-        queue<TreeNode*> q;
-        q.push(root);
-        m[root]=NULL;
-        while(!q.empty())
-        {
-            int n=q.size();
-            while(n--)
-            {
-                auto node=q.front();
-                q.pop();
-                if(node->left)
-                {
-                    m[node->left]=node;
-                    q.push(node->left);
-                }
-                if(node->right)
-                {
-                    m[node->right]=node;
-                    q.push(node->right);
-                }
-            }
-        }
-        int ans=0;
-        for(auto i:m)
-        {
-            auto child=i.first,par=i.second;
-            //cout<<child->val<<"\n";
-            if(par!=NULL)
-            {
-                if(m[par] && m[par]->val%2==0){
-                    ans+=child->val;
-                    //cout<<child->val<<" "<<m[par]->val<<"\n";
-                }
-            }
-        }
+        dfs(root,NULL,NULL);
         return ans;
     }
 };
