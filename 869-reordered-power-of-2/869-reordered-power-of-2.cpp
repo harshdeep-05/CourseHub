4921f67d3fe574113980362b845744994b4e1232
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-04-07 01:52:42 +0530

    Time: 3 ms (66.78%), Space: 6.3 MB (41.61%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> count(int n)
+    {
+        vector<int> v(10,0);
+        while(n)
+        {
+            v[n%10]++;
+            n/=10;
+        }
+        return v;
+    }
+    
+    bool reorderedPowerOf2(int n) {
+        vector<int> v=count(n);
+        for(int i=0;i<31;i++)
+        {
+            if(v==count(1<<i))return true;
+        }
+        return false;
+        
+        
+    }
+};
