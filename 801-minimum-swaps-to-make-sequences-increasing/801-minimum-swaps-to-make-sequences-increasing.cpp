6ec6f5a85eb81a2c9f8efc772318e977053b9561
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-04-12 12:19:11 +0530

    Time: 219 ms (68.97%), Space: 94.3 MB (42.29%) - LeetHub

--- 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     vector<vector<int>> dp;
     int solve(vector<int>& nums1, vector<int>& nums2,int i,bool swapped,int prev1,int prev2)
     {
@@ -17,8 +18,27 @@ public:
         swap(nums1[i],nums2[i]);
         return dp[i][swapped]=ans;
     }
+    */
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
-        dp.resize(nums1.size(),vector<int>(2,-1));
-        return solve(nums1,nums2,0,0,-1,-1);
+        //dp.resize(nums1.size(),vector<int>(2,-1));
+        int n=nums1.size();
+        vector<int> swap(n,1),not_swap(n,0);
+        for(int i=1;i<n;i++)
+        {
+            swap[i]=INT_MAX;not_swap[i]=INT_MAX;
+            if(nums1[i]>nums1[i-1] && nums2[i]>nums2[i-1])
+            {
+                not_swap[i]=not_swap[i-1];
+                swap[i]=swap[i-1]+1;
+            }
+            if(nums1[i]>nums2[i-1] && nums2[i]>nums1[i-1])
+            {
+                not_swap[i]=min(swap[i-1],not_swap[i]);
+                swap[i]=min(not_swap[i-1]+1,swap[i]);
+            }
+        }
+        return min(swap[n-1],not_swap[n-1]);
+        
+        //return solve(nums1,nums2,0,0,-1,-1);
     }
 };
