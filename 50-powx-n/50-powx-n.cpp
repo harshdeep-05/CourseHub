47ddac4f135383d1ca6c9f3b581c8be434ed8cb8
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-04-13 17:12:47 +0530

    Time: 0 ms (100.00%), Space: 6 MB (45.09%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        if(x==1.0 || n==0)return 1.0;
+        if (x == -1) {
+            if (n % 2 == 0) return 1;
+            else return -1;
+        }
+        if (n == INT_MIN) return 0;
+        if(n<0)
+        {
+            x=1.0/x;
+            n=-n;
+        }
+        double ans=1;
+        while(n)
+        {
+            if(n&1)ans*=x;
+            x*=x;
+            n/=2;
+        }
+        return ans;
     }
 };
