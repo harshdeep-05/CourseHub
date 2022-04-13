f649ad3974dc5dc3d2c05e354d739e5110c530b9
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-04-13 17:49:49 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (53.46%) - LeetHub

--- 788-rotated-digits/788-rotated-digits.cpp
+++ 788-rotated-digits/788-rotated-digits.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-    int solve(int x,map<int,int> &m)
-    {
-        int num=0,i=0;
-        while(x)
-        {
-            int d=x%10;
-            if(d==4 || d==3 || d==7)return -1;
-            else num+=m[d]*pow(10,i++);
-            x/=10;
-        }
-        return num;
-    }
-    
     int rotatedDigits(int n) {
         int ans=0;
-        map<int,int> m;
-        m[0]=0;m[1]=1;m[2]=5;m[5]=2;m[6]=9;m[8]=8;m[9]=6;
+        vector<int> rot={1,1,2,0,0,2,2,0,1,2};
         for(int i=1;i<=n;i++)
         {
-            int y=solve(i,m);
-            cout<<y<<" ";
-            if(i!=y && y!=-1)ans++;
+            int num=i,s=1;
+            while(num)
+            {
+                s*=rot[num%10];
+                num/=10;
+            }
+            if(s>=2)ans++;
         }
         return ans;
     }
