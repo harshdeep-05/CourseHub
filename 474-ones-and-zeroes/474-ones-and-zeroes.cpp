57f3d5cbd56be1ff6a323d4f9fc20b28cafc1acb
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-05-23 11:54:15 +0530

    Time: 731 ms (16.17%), Space: 35.7 MB (51.78%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,57 +1,22 @@
 class Solution {
 public:
-    /*
-    int dp[601][101][101];
-    int solve(vector<string>& strs,int i, int m, int n,unordered_map<string,pair<int,int>> &cnt)
+    int dp[101][101][601];
+    int solve(vector<string>& strs, int m, int n,int index)
     {
-        if(i<0)return 0;
-        if(dp[i][m][n]!=-1)return dp[i][m][n];
-        auto x=cnt[strs[i]];
+        if(index<0)return 0;
+        int count_0=count(strs[index].begin(),strs[index].end(),'0');
+        int count_1=count(strs[index].begin(),strs[index].end(),'1');
         
-        if(x.first>m || x.second>n)
-            return dp[i][m][n]=solve(strs,i-1,m,n,cnt);
-        else
-            return dp[i][m][n]=max(solve(strs,i-1,m,n,cnt),solve(strs,i-1,m-x.first,n-x.second,cnt)+1);
+        if(dp[m][n][index]!=-1)return dp[m][n][index];
+        if(count_0<=m && count_1<=n )
+        {
+            return dp[m][n][index]=max(solve(strs,m,n,index-1),solve(strs,m-count_0,n-count_1,index-1)+1);
+        }
+        return dp[m][n][index]=solve(strs,m,n,index-1);
     }
-    */
     int findMaxForm(vector<string>& strs, int m, int n) {
-        /*
-        unordered_map<string,pair<int,int>> cnt;
+        int index=strs.size();
         memset(dp,-1,sizeof dp);
-        for(auto i:strs)
-        {
-            int a=0,b=0;
-            for(auto c:i)
-            {
-                if(c=='0')a++;
-                else b++;
-            }
-            cnt[i]={a,b};
-        }
-        return solve(strs,strs.size()-1,m,n,cnt);
-        */
-        
-        int dp[m+1][n+1];
-        memset(dp,0,sizeof dp);
-        for(auto s:strs)
-        {
-            int a=0,b=0;
-            for(auto c:s)
-            {
-                if(c=='0')a++;
-                else b++;
-            }
-            
-            for(int i=m;i>=0;i--)
-            {
-                for(int j=n;j>=0;j--)
-                {
-                    if(a<=i && b<=j)
-                        dp[i][j]=max(dp[i][j],dp[i-a][j-b]+1);
-                }
-            }
-        }
-        return dp[m][n];
-        
+        return solve(strs,m,n,index-1);
     }
 };
