abb50b0fafc1da6a829498ac0ae19c5ca350a065
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-06-04 00:28:18 +0530

    Time: 3 ms (41.37%), Space: 5.9 MB (82.74%) - LeetHub

---
+++ 738-monotone-increasing-digits/738-monotone-increasing-digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int monotoneIncreasingDigits(int n) {
+        string num=to_string(n);
+        int flag=num.size();
+        for(int i=num.size()-1;i>0;i--)
+        {
+            if(num[i]<num[i-1])
+            {
+                flag=i;
+                num[i-1]--;
+            }
+        }
+        for(int i=flag;i<num.size();i++)
+        {
+            num[i]='9';
+        }
+        return stoi(num);
+    }
+};
