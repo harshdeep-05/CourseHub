a6ec06e97ff31100062846410781302ed7b30aeb
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-06-21 00:05:04 +0530

    Time: 86 ms (64.85%), Space: 73.7 MB (33.66%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,23 +1,44 @@
+struct Node{
+    Node* list[26];
+    int count=0;
+    string word;
+    
+    Node* get(char c)
+    {
+        if(!list[c-'a'])
+        {
+            count++;
+            list[c-'a']=new Node();
+        }
+        return list[c-'a'];
+    }
+    int collect(Node *node)
+    {
+        if(!node)return 0;
+        if(node->count==0)return node->word.size()+1;
+        
+        int count=0;
+        for(auto n:node->list)count+=collect(n);
+        return count;
+    }
+    void add(Node* trie,string s)
+    {
+        Node* node=trie;
+        for(int i=s.size()-1;i>=0;i--)
+            node=node->get(s[i]);
+        
+        node->word=s;
+    }
+    
+    
+};
 class Solution {
 public:
     int minimumLengthEncoding(vector<string>& words) {
-        set<string> s;
-        for(auto word:words)
-        {
-            s.insert(word);
+        Node* trie=new Node();
+        for(auto s:words){
+            trie->add(trie,s);
         }
-        for(auto word:words)
-        {
-            for(int i=1;i<word.size();i++)
-            {
-                s.erase(word.substr(i));
-            }
-        }
-        int ans=0,n=0;
-        for(auto i:s)
-        {
-            ans+=i.size()+1;
-        }
-        return ans;
+        return trie->collect(trie);
     }
 };
