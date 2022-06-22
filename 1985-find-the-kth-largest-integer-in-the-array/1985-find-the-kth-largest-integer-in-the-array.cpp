dbde989d639ae9253db57f1f76b16d13e6b896a3
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-06-22 22:16:51 +0530

    Time: 1318 ms (5.97%), Space: 336.4 MB (5.97%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
@@ -4,17 +4,17 @@ public:
     {
         int i=0;
         if(a.size()!=b.size())return a.size()<b.size();
+        if(a==b)return 0;
         while(i<a.size() && a[i]==b[i])
         {
             i++;
         }
-        if(i==a.size())return 0;
         return a[i]<b[i];
     }
     string kthLargestNumber(vector<string>& nums, int k) {
         sort(nums.begin(),nums.end(),comp);
-        for(auto i:nums)cout<<i<<" ";
-        cout<<"\n";
+        //for(auto i:nums)cout<<i<<" ";
+        //cout<<"\n";
         return nums[nums.size()-k];
     }
 };
