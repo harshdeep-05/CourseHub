d2674ca20e21d83dc22f8c786ee6cb15dc65c721
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-07-23 16:14:55 +0530

    Time: 122 ms (79.64%), Space: 14.7 MB (84.40%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -2,30 +2,29 @@ class Solution {
 public:
     bool check(vector<int> &v,vector<int> &c)
     {
-        long long x=pow(v[0]-c[0],2);
-        long long y=pow(v[1]-c[1],2);
-        return (x+y<=pow(v[2],2));
+        return pow(v[0]-c[0],2)+pow(v[1]-c[1],2)<=pow(v[2],2);
     }
-    void dfs(vector<vector<int>> &graph,int node,vector<bool> &vis,int &cnt)
+    void dfs(vector<int> graph[],int node,vector<bool> &vis,int &cnt)
     {
         vis[node]=true;
-        for(int j=0;j<graph.size();j++)
+        for(auto v:graph[node])
         {
-            if(!vis[j] && graph[node][j]==1){
+            if(!vis[v]){
                 cnt++;
-                dfs(graph,j,vis,cnt);
+                dfs(graph,v,vis,cnt);
             }
         }
     }
     int maximumDetonation(vector<vector<int>>& bombs) {
         int n=bombs.size();
-        vector<vector<int>> graph(n,vector<int> (n,0));
+        vector<int> graph[n];
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<n;j++)
             {
+                if(i==j)continue;
                 if(check(bombs[i],bombs[j]))
-                    graph[i][j]=1;
+                    graph[i].push_back(j);
             }
         }
         int ans=0;
