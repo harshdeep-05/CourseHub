5da42a973139827318a7946ea2f4068136cc2899
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-07-25 17:03:37 +0530

    Time: 15 ms (39.90%), Space: 13.6 MB (83.66%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,44 +1,33 @@
 class Solution {
 public:
-    int first(vector<int>& nums, int target)
-    {
-        int l=0,r=nums.size()-1,m,res=-1;
-        while(l<=r)
-        {
-            m=l+(r-l)/2;
-            if(nums[m]==target)
-            {
-                res=m;
-                r=m-1;
-            }
-            else if(nums[m]<target)l=m+1;
-            else r=m-1;
-        }
-        return res;
-        
-    }
-    
-    int last(vector<int>& nums, int target)
-    {
-        int l=0,r=nums.size()-1,m,res=-1;
-        while(l<=r)
-        {
-            m=l+(r-l)/2;
-            if(nums[m]==target)
-            {
-                res=m;
-                l=m+1;
-            }
-            else if(nums[m]<target)l=m+1;
-            else r=m-1;
-        }
-        return res;
-        
-    }
-    
     vector<int> searchRange(vector<int>& nums, int target) {
-        return {first(nums,target),last(nums,target)};
+        int n=nums.size();
+        int l=0,r=n-1;
+        int low=-1,high=-1;
         
-        
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target)
+            {
+                high=mid;
+                l=mid+1;
+            }
+            else if(nums[mid]<target)l=mid+1;
+            else r=mid-1;
+        }
+        l=0,r=n-1;
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target)
+            {
+                low=mid;
+                r=mid-1;
+            }
+            else if(nums[mid]<target)l=mid+1;
+            else r=mid-1;
+        }
+        return {low,high};
     }
 };
