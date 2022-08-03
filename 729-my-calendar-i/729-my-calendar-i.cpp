25797f6ae7edd13e386d6afc3dfb4eb894d78747
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-08-03 16:36:19 +0530

    Time: 369 ms (12.64%), Space: 38.5 MB (73.48%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -0,0 +1,24 @@
+class MyCalendar {
+    unordered_map<int,int> m;
+public:
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+        for(auto i:m)
+        {
+            int x=i.first,y=i.second;
+            if(x<end && y>start)
+                return false;
+        }
+        m[start]=end;
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
