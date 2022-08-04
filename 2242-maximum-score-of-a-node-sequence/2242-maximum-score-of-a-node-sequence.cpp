06ad9d1567c3ead937cdcc244b67ba99947a4a09
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-08-04 14:32:47 +0530

    Time: 735 ms (72.38%), Space: 153.8 MB (41.94%) - LeetHub

--- 2242-maximum-score-of-a-node-sequence/2242-maximum-score-of-a-node-sequence.cpp
+++ 2242-maximum-score-of-a-node-sequence/2242-maximum-score-of-a-node-sequence.cpp
@@ -2,29 +2,32 @@ class Solution {
 public:
     int maximumScore(vector<int>& scores, vector<vector<int>>& edges) {
         int n=scores.size();
-        vector<vector<pair<int,int>>>G(n);
-        for(auto &x:edges)
+        vector<vector<pair<int,int>>> graph(n);
+        for(auto &v:edges)
         {
-            G[x[0]].push_back({scores[x[1]],x[1]});
-            G[x[1]].push_back({scores[x[0]],x[0]});
+            graph[v[0]].push_back({scores[v[1]],v[1]});
+            graph[v[1]].push_back({scores[v[0]],v[0]});
         }
-        for(int i=0;i<n;i++)
-            sort(G[i].begin(),G[i].end(),greater<pair<int,int>>());
-        int ans=-1;
-        for(auto &x:edges)
+        for(auto &v:graph)
         {
-            int left=x[0],right=x[1];
-            int cur=scores[left]+scores[right];
-            int sz1=G[left].size(),sz2=G[right].size();
+            sort(v.begin(),v.end(),greater<pair<int,int>> ());
+        }
+        int ans=-1;
+        for(auto x:edges)
+        {
+            int u=x[0],v=x[1];
+            int sz1=graph[u].size(),sz2=graph[v].size();
+            int cur=scores[u]+scores[v];
             for(int i=0;i<min(3,sz1);i++)
+            {
                 for(int j=0;j<min(3,sz2);j++)
                 {
-                    if(G[left][i].second==left||G[left][i].second==right||G[left][i].second==G[right][j].second)
-                        continue;
-                    if(G[right][j].second==left||G[right][j].second==right||G[right][j].second==G[left][i].second)
-                        continue;
-                    ans=max(ans,cur+G[left][i].first+G[right][j].first);
+                    if(graph[u][i].second!=graph[v][j].second && graph[u][i].second!=v && graph[v][j].second!=u)
+                    {
+                        ans=max(ans,cur+graph[u][i].first+graph[v][j].first);
+                    }
                 }
+            }
         }
         return ans;
         
