97772e7c470e16d975a68b952a6af7bc1b59ae03
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-08-08 20:37:18 +0530

    Time: 9 ms (94.29%), Space: 10.5 MB (47.88%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
-    int dp[2505][2505];
-    int solve(vector<int> &nums,int n,int prev)
-    {
-        if(n<0)
-            return 0;
-        
-        if(dp[n][prev]!=-1)
-            return dp[n][prev];
-        
-        int inc=0,no_inc=0;
-        
-        if(prev==nums.size() || nums[n]<nums[prev])
-            inc=1+solve(nums,n-1,n);
-        
-        no_inc=solve(nums,n-1,prev);
-        
-        return dp[n][prev]=max(inc,no_inc);
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        memset(dp,-1,sizeof dp);
-        return solve(nums,n-1,n);
+        vector<int> v;
+        for(auto i:nums)
+        {
+            if(v.empty() || i>v.back())
+                v.push_back(i);
+            else
+            {
+                /*
+                int l=0,r=v.size()-1;
+                while(l<r)
+                {
+                    int m=l+(r-l)/2;
+                    if(v[m]<=i)
+                        r=m-1;
+                    else l=m+1;
+                }
+                if(l < v.size() && v[l] < i) {
+                    l++;
+                }
+                */
+                int l=lower_bound(v.begin(),v.end(),i)-v.begin();
+                v[l]=i;
+            }
+            
+        }
+        return v.size();
         
         
     }
