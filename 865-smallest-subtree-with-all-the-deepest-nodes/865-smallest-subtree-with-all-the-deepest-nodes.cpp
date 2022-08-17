299ffc13b3fa7bff2e1e875809a3b524423fc7d1
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-08-17 22:50:58 +0530

    Time: 0 ms (100.00%), Space: 14.3 MB (95.44%) - LeetHub

--- 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -11,45 +11,27 @@
  */
 class Solution {
 public:
-    TreeNode* lca(TreeNode* root,TreeNode* a,TreeNode* b)
+    int deep(TreeNode* root)
     {
-        if(!root)
-            return NULL;
+        if(!root)return 0;
         
-        if(root==a || root==b)return root;
+        return max(deep(root->left),deep(root->right))+1;
+    }
+    TreeNode* lca(TreeNode* root,int &depth,int h)
+    {
+        if(!root || depth==h)
+            return root;
         
-        auto l=lca(root->left,a,b);
-        auto r=lca(root->right,a,b);
+        auto l=lca(root->left,depth,h+1);
+        auto r=lca(root->right,depth,h+1);
         
-        if(l && r)return root;
-        
-        if(!l)return r;
-        return l;
+        if(l&&r)return root;
+        if(l)return l;
+        return r;
     }
     TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        // int last_size=0;
-        queue<TreeNode*> q;
-        q.push(root);
-        vector<TreeNode*> v;
-        while(!q.empty())
-        {
-            int s=q.size();
-            // last_size=s;
-            v.clear();
-            while(s--)
-            {
-                auto top=q.front();
-                q.pop();
-                v.push_back(top);
-                if(top->left)q.push(top->left);
-                if(top->right)q.push(top->right);
-            }
-        }
-        // cout<<v.size()<<" ";
-        if(v.size()==1)return v[0];
-        TreeNode* a=v[0],*b=v.back();
-        
-        return lca(root,a,b);
-        // return NULL;
+        int depth=deep(root);
+        // cout<<depth;
+        return lca(root,depth,1);
     }
 };
