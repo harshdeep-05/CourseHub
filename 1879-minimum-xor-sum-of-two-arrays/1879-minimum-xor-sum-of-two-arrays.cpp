30e59777de6c984026391bb0801b8328dabe10f5
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-08-18 14:40:24 +0530

    Time: 56 ms (31.11%), Space: 9.6 MB (71.56%) - LeetHub

---
+++ 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int calc(int state, int n, vector<int>& dp, vector<int>& ar, vector<int>& br) {
+        if(dp[state] != -1) return dp[state];
+        if(state == 0) return 0;
+        dp[state] = INT_MAX;
+        int ns = 0;
+        int dum = state;
+        while(dum) {
+            if(dum % 2 == 1) {
+                ns++;
+            }
+            dum /= 2;
+        }
+        for(int i = 0; i < n; i++) {
+            if((state & (1 << i)) != 0) {
+                dp[state] = min(dp[state], calc((state ^ (1 << i)), n, dp,ar,br) + (ar[ns - 1] ^ br[i]));
+            }
+        }
+        // if(dp[state] == INT_MAX){
+        //     cout<<"grfd\n";
+        //     return dp[state] = 1;
+        // }
+        return dp[state];
+    }
+    int minimumXORSum(vector<int>& ar, vector<int>& br) {
+        int n=ar.size();
+        vector<int> ans((1<<n), -1);
+        return calc((1 << n) - 1, n, ans, ar, br);
+    }
+};
