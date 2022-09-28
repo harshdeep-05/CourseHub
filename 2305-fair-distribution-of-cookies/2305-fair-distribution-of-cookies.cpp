bc10e84161f8d819cd264a705b826e290937bd99
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-09-28 22:29:37 +0530

    Time: 18 ms (67.41%), Space: 7.2 MB (12.33%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
-    int ans=INT_MAX;
-    void solve(int i,int k,vector<int> &cookies,vector<int>& v)
-    {
-        if(i==cookies.size())
+    int distributeCookies(vector<int>& cookies, int k) {
+        int n=cookies.size(),sz=1<<n;
+        
+        vector<vector<int>> dp(k+1,vector<int> (sz,INT_MAX));
+        vector<int> sum(sz,0);
+        
+        for(int mask=0;mask<sz;mask++)
         {
-            int mx=0;
-            for(auto x:v)mx=max(x,mx);
-            ans=min(ans,mx);
-            return;
+            for(int i=0;i<n;i++)
+            {
+                if(mask & (1<<i))
+                {
+                    sum[mask]+=cookies[i];
+                }
+            }
         }
         
-        for(int j=0;j<k;j++)
+        dp[0][0]=0;
+        for(int set=1;set<=k;set++)
         {
-            v[j]+=cookies[i];
-            solve(i+1,k,cookies,v);
-            v[j]-=cookies[i];
+            for(int mask=0;mask<sz;mask++)
+            {
+                for(int i=mask;i>0;i=(i-1)&mask)
+                {
+                    dp[set][mask]=min(dp[set][mask],max(sum[i],dp[set-1][mask^i]));
+                }
+            }
         }
-        return;
-    }
-    int distributeCookies(vector<int>& cookies, int k) {
-        vector<int> v(k,0);
-        solve(0,k,cookies,v);
-        return ans;
+        return dp[k][sz-1];
     }
 };
