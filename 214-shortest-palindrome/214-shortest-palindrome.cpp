3e25b9c4ab2c35918210ea10bbee1f60a5d2d5f4
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-10-01 19:32:44 +0530

    Time: 3 ms (92.77%), Space: 7.1 MB (94.43%) - LeetHub

--- 214-shortest-palindrome/214-shortest-palindrome.cpp
+++ 214-shortest-palindrome/214-shortest-palindrome.cpp
@@ -2,20 +2,29 @@ class Solution {
 public:
     string shortestPalindrome(string s) {
         int n=s.size();
-        string rev=s;
-        reverse(rev.begin(),rev.end());
-        string a=s+'#'+rev;
-        int m=a.size();
-        vector<int> f(m,0);
-        for(int i=1;i<m;i++)
-        {
-            int j=f[i-1];
-            while(j>0 && a[i]!=a[j])j=f[j-1];
-            
-            if(a[i]==a[j])j++;
-            f[i]=j;
-        }
-        return rev.substr(0,n-f[m-1])+s;
+        if(n<2)return s;
+        int prime=29,mod=1e9+7;
+        long long int p=prime;
+        long long int shash=s[0]-'a'+1,rhash=s[0]-'a'+1;
         
+        int index=0;
+        for(int i=1;i<n;i++)
+        {
+            int c=s[i]-'a'+1;
+            shash=shash+c*p;
+            shash%=mod;
+            rhash=rhash*prime+c;
+            rhash%=mod;
+            
+            p=(p*prime)%mod;
+            if(shash==rhash)
+            {
+                index=i;
+            }
+        }
+        
+        string rev=s.substr(index+1);
+        reverse(rev.begin(),rev.end());
+        return rev+s;
     }
 };
