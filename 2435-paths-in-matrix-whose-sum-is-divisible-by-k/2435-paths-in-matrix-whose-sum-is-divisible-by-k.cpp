432ae94311bf8ea5913082b9d7056bc571a8cb73
Harshdeep Sahu <76677268+harshdeep-05@users.noreply.github.com>
2022-10-09 19:25:32 +0530

    Time: 1069 ms (22.22%), Space: 113.6 MB (100.00%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     const int mod=1e9+7;
-    vector<vector<vector<int>>> dp;
+    vector<vector<int>> dp;
     int x,m,n;
     int solve(vector<vector<int>>& v,int i,int j,int rem)
     {
@@ -10,19 +10,21 @@ public:
         {
             return (rem==0);
         }
-        if(dp[i][j][rem]!=-1)
-        {
-            return dp[i][j][rem];
-        }
+        // if(dp[i][j][rem]!=-1)
+        // {
+        //     return dp[i][j][rem];
+        // }
+        if(dp[i*n+j][rem]!=-1)return dp[i*n+j][rem];
         int ans=0;
         if(i+1<m)ans+=solve(v,i+1,j,rem);
         if(j+1<n)ans+=solve(v,i,j+1,rem);
-        return dp[i][j][rem]=ans%mod;
+        return dp[i*n+j][rem]=ans%mod;
     }
     int numberOfPaths(vector<vector<int>>& grid, int k) {
         m=grid.size(),n=grid[0].size();
         x=k;
-        dp=vector<vector<vector<int>>> (m,vector<vector<int>>(n,vector<int> (k,-1)));
+        // dp=vector<vector<vector<int>>> (m,vector<vector<int>>(n,vector<int> (k,-1)));
+        dp=vector<vector<int>> (m*n,vector<int> (k,-1));
         return solve(grid,0,0,0);
     }
 };
